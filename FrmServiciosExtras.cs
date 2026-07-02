e85c8a49b00985d7ca3fc797978c06fad913b9b0
jagallegosdanda-max <jagallegosdanda@gmail.com>
2026-07-02 00:08:43 -0600

    prueba

---
+++ FrmServiciosExtras.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Administracion_Hotel
+{
+    public partial class FrmServiciosExtras : Form
+    {
+        public FrmServiciosExtras()
+        {
+            InitializeComponent();
+        }
+    }
+}
